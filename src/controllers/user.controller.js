9295bf030648111c0b7c22056d885bd748ec554b
Alexis Pizarro <alexpizarro3@gmail.com>
2022-08-08 19:13:40 -0600

    added productos CRUD

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -77,6 +77,82 @@ const updateUsuario = async (req, res, next) => {
     }
 }
 
+const getAllProductos = async (req, res, next) => {
+    try {
+        const resultProductos = await pool.query(`SELECT * FROM "Users"."Productos"`);
+        res.json(resultProductos.rows);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const gelAllTipoProducto = async (req, res, next) => {
+    try {
+        const resultTipoProducto = await pool.query(`SELECT * FROM "Users"."TipoProducto"`);
+        res.json(resultTipoProducto.rows);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getProductoById = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`SELECT * FROM "Users"."Productos", "Users"."TipoProducto" 
+                                    WHERE "Tipo" = "idTipo" AND "IdProducto" = '${id}'`);
+        if (result.rows.length === 0) return res.status(404).json({
+            message: "Producto no esta en lista"
+        })
+        res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const crearProducto = async (req, res, next) => {
+    const { Nombre, Descripcion, Tipo, PrecioVenta, PrecioCompra } = req.body;
+    try {
+        const resultInsert = await pool.query(`INSERT INTO "Users"."Productos"("Nombre", "Descripcion", "Tipo", "PrecioVenta", "PrecioCompra", "Inventario")
+        VALUES ('${Nombre}', '${Descripcion}', '${Tipo}', '${PrecioVenta}', '${PrecioCompra}', '0') RETURNING *;`);
+        res.json(resultInsert.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const borrarProducto = async (req, res, next) => {
+    const { id } = req.params; //Asignamos el id
+    try {
+        const result = await pool.query(`DELETE FROM "Users"."Productos" 
+                                    WHERE "IdProducto" = '${id}' RETURNING *`);
+        if (result.rowCount === 0) return res.status(404).json({
+            message: "Producto no encontrado"
+        })
+        res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const updateProducto = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { Nombre, Descripcion, Tipo, PrecioVenta, PrecioCompra, Inventario } = req.body;
+        const result = await pool.query(`UPDATE "Users"."Productos" SET "Nombre" = '${Nombre}', 
+                                    "Descripcion" = '${Descripcion}', "Tipo" = '${Tipo}', "PrecioVenta" = '${PrecioVenta}',
+                                    "PrecioCompra" = '${PrecioCompra}', "Inventario" = '${Inventario}' 
+                                    WHERE "IdProducto" = '${id}' RETURNING *;`);
+        if (result.rows.length === 0) return res.status(404).json({
+            message: "Producto no encontrado"
+        });
+        return res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+
 module.exports = {
     getAllUsers,
     pagInicio,
@@ -85,4 +161,10 @@ module.exports = {
     crearUsuario,
     borrarUsuario,
     updateUsuario,
+    getAllProductos,
+    gelAllTipoProducto,
+    getProductoById,
+    crearProducto,
+    borrarProducto,
+    updateProducto,
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { getAllUsers, pagInicio, getUserById, gelAllRoles, crearUsuario, borrarUsuario, updateUsuario} = require('../controllers/user.controller');
+const { getAllUsers, pagInicio, getUserById, gelAllRoles, crearUsuario, borrarUsuario, updateUsuario, getAllProductos, gelAllTipoProducto, getProductoById, crearProducto, borrarProducto, updateProducto} = require('../controllers/user.controller');
 
 const pool = require('../db');
 const router = Router();
@@ -11,5 +11,12 @@ router.get('/users/:id', getUserById);
 router.post('/crearUser', crearUsuario);
 router.delete('/borrarUser/:id', borrarUsuario);
 router.put('/upUser/:id', updateUsuario);
+
+router.get('/productos', getAllProductos);
+router.get('/productos/tipos', gelAllTipoProducto);
+router.get('/productos/:id', getProductoById);
+router.post('/crearProducto', crearProducto);
+router.delete('/borrarProducto/:id', borrarProducto);
+router.put('/upProducto/:id', updateProducto);
 
 module.exports = router;
